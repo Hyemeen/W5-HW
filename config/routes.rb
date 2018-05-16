4ccdccbd7a3bba1289099801c9b128a1f44eb281
Hyemeen <poam131@likelion.org>
2018-05-16 22:36:55 +0900

    post

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+
+  resources :posts
+  root 'posts#index'
+
+  # post 'posts/create' => 'posts#create', as: 'posts'
+
+  # get 'posts/new'
+
+  # get 'posts/index'
+
+  # get 'posts/show/:post_id' => 'posts#show'
+
+  # patch 'posts/update/:post_id' => 'posts#update', as: 'post'
+
+  # get 'posts/edit/:post_id' => 'posts#edit'
+
+  # get 'posts/destroy/:post_id' => 'posts#destroy'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
